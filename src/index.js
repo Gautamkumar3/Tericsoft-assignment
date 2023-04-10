be3f3881e9d3c95b2e2450a87ab65d28b60946b7
Gautam Kumarm <g2gautamsingh@gmail.com>
2023-04-10 22:00:02 +0530

    register and login route done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const dbConnect = require("./config/db");
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
+const userRouter = require("./route/user");
+
 const app = express();
+app.use(express.json());
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Tericsoft");
